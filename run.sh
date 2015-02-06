054e172b216bc4b51e86c1d9aa14a185e1867ba9
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2015-02-06 15:11:00 +0100

    step name

--- run.sh
+++ run.sh
@@ -6,23 +6,23 @@ if [ ! -n "SLACK_URL" ]; then
 fi
 
 # get the channel name to post notifications to
-if [ ! -n "SLACK_CHANNEL" ]; then
+if [ ! -n "SLACK_NOTIFIER_CHANNEL" ]; then
     error "Please provide a Slack channel to push notifications to"
 fi
 
 # check if a '#' was supplied in the channel name
-if [[ $WERCKER_SLACK_NOTIFY_CHANNEL == \#* ]]; then
+if [[ $SLACK_NOTIFIER_CHANNEL == \#* ]]; then
   error "Please specify the channel without the '#'"
 fi
 
 # if no username is provided use the default - werckerbot
-if [ ! -n "$WERCKER_SLACK_USERNAME" ]; then
-  export WERCKER_SLACK_USERNAME=werckerbot
+if [ ! -n "$SLACK_NOTIFIER_USERNAME" ]; then
+  export SLACK_NOTIFIER_USERNAME=werckerbot
 fi
 
 # if no icon-url is provided for the bot use the default wercker icon
-if [ ! -n "$WERCKER_SLACK_ICON_URL" ]; then
-    export WERCKER_SLACK_ICON_URL="https://secure.gravatar.com/avatar/a08fc43441db4c2df2cef96e0cc8c045?s=140"
+if [ ! -n "$SLACK_NOTIFIER_ICON_URL" ]; then
+    export SLACK_NOTIFIER_ICON_URL="https://secure.gravatar.com/avatar/a08fc43441db4c2df2cef96e0cc8c045?s=140"
 fi
 
 # check if this event is a build or deploy
@@ -40,11 +40,18 @@ export MESSAGE="$ACTION for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has
 
 # construct the json
 json="{
-    \"channel\": \"$WERCKER_SLACK_NOTIFY_CHANNEL\",
-    \"username\": \"$WERCKER_SLACK_USERNAME\",
+    \"channel\": \"$SLACK_NOTIFIER_CHANNEL\",
+    \"username\": \"$SLACK_NOTIFIER_USERNAME\",
     \"text\":\"$MESSAGE\",
-    \"icon_url\":\"$WERCKER_SLACK_ICON_URL\"
+    \"icon_url\":\"$SLACK_NOTIFIER_ICON_URL\"
 }"
+
+# skip notifications if not interested in passed builds or deploys
+if [ "$NOTIFY_ON" = "failed" ]; then
+	if [ "$WERCKER_RESULT" = "passed" ]; then
+		return 0
+	fi
+fi
 
 # post the result to the slack webhook
 RESULT=`curl -d "payload=$json" -s "$SLACK_URL" --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
